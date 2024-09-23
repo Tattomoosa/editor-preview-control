cd1a2eb7901d012f1c618301223a5c68c4968121
Matt O'Tousa <mlotousa@gmail.com>
2024-09-22 19:42:22 -0700

    no more error messages

--- addons/tattomoosa.editor-control-preview/plugin.gd
+++ addons/tattomoosa.editor-control-preview/plugin.gd
@@ -3,13 +3,11 @@ extends EditorPlugin
 
 var bottom_panel_control := preload("./src/editor_control_preview.tscn").instantiate()
 
-func _enter_tree():
+@warning_ignore("return_value_discarded")
+func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	bottom_panel_control.editor_interface = get_editor_interface()
 	add_control_to_bottom_panel(bottom_panel_control, "Control Preview")
-	pass
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_control_from_bottom_panel(bottom_panel_control)
-	pass
--- addons/tattomoosa.editor-control-preview/src/editor_control_preview.gd
+++ addons/tattomoosa.editor-control-preview/src/editor_control_preview.gd
@@ -6,43 +6,43 @@ extends Control
 @onready var clear_button : Button = %ClearButton
 @onready var root : Control = %Root
 
-var editor_interface : EditorInterface
+const DUPLICATE_FLAGS = DuplicateFlags.DUPLICATE_USE_INSTANTIATION | DuplicateFlags.DUPLICATE_SCRIPTS
 
-func _ready():
-	load_selected_control_button.disabled = editor_interface == null
+@warning_ignore("return_value_discarded")
+func _ready() -> void:
+	load_selected_control_button.disabled = EditorInterface == null
 	load_selected_control_button.icon = get_theme_icon("ToolSelect", "EditorIcons")
 	load_selected_control_button.pressed.connect(_load_selected_control)
 	load_edited_scene_root_button.pressed.connect(_load_scene_root)
 	load_edited_scene_root_button.icon = get_theme_icon("PackedScene", "EditorIcons")
 	clear_button.pressed.connect(_clear)
 
-	editor_interface.get_selection().selection_changed.connect(
-		func():
-			if editor_interface.get_selection().get_selected_nodes().is_empty():
-				load_selected_control_button.disabled = true
-			else:
-				load_selected_control_button.disabled = false
-	)
+	EditorInterface.get_selection().selection_changed.connect(_on_selection_changed)
 
-func _clear():
+func _on_selection_changed() -> void:
+	if EditorInterface.get_selection().get_selected_nodes().is_empty():
+		load_selected_control_button.disabled = true
+	else:
+		load_selected_control_button.disabled = false
+
+func _clear() -> void:
 	for child in root.get_children(true):
-		remove_child(child)
+		root.remove_child(child)
 		child.queue_free()
 
-func _load_selected_control():
+func _load_selected_control() -> void:
 	_clear()
-	var selected := editor_interface.get_selection().get_selected_nodes()
+	var selected := EditorInterface.get_selection().get_selected_nodes()
 	if selected.size() > 1:
 		push_warning("Previewing multiple nodes may have unintended consequences")
 	for node in selected:
 		_add_node(node)
 
-func _load_scene_root():
+func _load_scene_root() -> void:
 	_clear()
-	var scene_root := editor_interface.get_edited_scene_root()
+	var scene_root := EditorInterface.get_edited_scene_root()
 	_add_node(scene_root)
 
-func _add_node(node: Node):
-	node = node.duplicate()
-	node.set("editor_interface", editor_interface)
+func _add_node(node: Node) -> void:
+	node = node.duplicate(DUPLICATE_FLAGS)
 	root.add_child(node)
